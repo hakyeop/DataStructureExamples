38b797fd94daa7a0e220284f91f20e7e0174f714
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-10-27 12:16:50 +0900

    Create 2-4.c

---
+++ chapter02/Array/2-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    char str[20] = "Data Structure!";
+    int i;
+    printf("\nString array str[] : ");
+    for(i=0; str[i]; i++){
+        printf("%c", str[i]);
+    }
+    getchar();
+}
